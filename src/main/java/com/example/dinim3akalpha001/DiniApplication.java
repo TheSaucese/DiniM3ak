82c3e23cc596601dbf4b9a8c7df227038bd7f8cd
TheSaucese <selfarricha2003@gmail.com>
2022-12-19 10:24:38 +0100

    version 2

--- src/main/java/com/example/dinim3akalpha001/DiniApplication.java
+++ src/main/java/com/example/dinim3akalpha001/DiniApplication.java
@@ -11,12 +11,14 @@ import java.io.IOException;
 public class DiniApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(DiniApplication.class.getResource("Signup.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(DiniApplication.class.getResource("ProfileDriver.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 375, 812);
         scene.getStylesheets().add(DiniApplication.class.getResource("style.css").toExternalForm());
         stage.setTitle("DINIM3AK3000");
         stage.setScene(scene);
         stage.setResizable(false);
+        stage.setX(25);
+        stage.setY(25);
         //stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
     }
